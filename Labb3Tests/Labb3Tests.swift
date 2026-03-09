6eb22de6874b5eae87235440e3a65fb01fd576c7
yazan <yazankh0115@gmail.com>
2026-03-09 01:01:45 +0100

    Initial Commit

---
+++ Labb3Tests/Labb3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Labb3Tests.swift
+//  Labb3Tests
+//
+//  Created by Yazan Khraiba on 2026-03-09.
+//
+
+import Testing
+@testable import Labb3
+
+struct Labb3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
